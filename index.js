6bf77c2733a467e539b8c631286e4b2583828ea1
Kang Yerim <yerim25@gmail.com>
2020-10-18 20:39:47 +0900

    protect private information

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const config = require("./config/key");
+const bodyParser = require("body-parser");
 const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://yerim25:na6175yj@cluster0.mjt1k.mongodb.net/Cluster0?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/api/users/register", (req, res) => {
+  // 회원가입할 때 필요한 정보들을 클라이언트에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
